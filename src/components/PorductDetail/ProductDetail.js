2ec889b93b3675bc8fc00b3a516bc5d484747bb7
Md. Abdullah <abdullahkafi996@gmail.com>
2021-03-09 02:01:51 +0600

    take product details form Product component

--- src/components/PorductDetail/ProductDetail.js
+++ src/components/PorductDetail/ProductDetail.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import fakeData from '../../fakeData';
+import Product from '../Product/Product';
 import './ProductDetail.css'
 
 const ProductDetail = () => {
+    const {productKey} = useParams();
+    const product= fakeData.find(pd => pd.key === productKey);
+    console.log(product);
     return (
         <div className="container">
-            <h2>Product Details Comming Soon....</h2>
+            <h2>{productKey} Details Comming Soon....</h2>
+            {/* add product details form product component */}
+            <Product product={product}></Product>
         </div>
     );
 };
